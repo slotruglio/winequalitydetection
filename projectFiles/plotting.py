ae196d2807c3c625b96c56a54822c75db3380b93
SAMUELE LO TRUGLIO s295285 <s295285@studenti.polito.it>
2022-06-22 16:07:37 +0200

    fix(plot), feat(plot_results)

--- projectFiles/plotting.py
+++ projectFiles/plotting.py
@@ -2,6 +2,7 @@
 from utilityML.Functions.genpurpose import load
 from utilityML.Functions.plot import *
 from utilityML.Functions.normalization import *
+from utilityML.Functions.dimred import pca
 
 # LOAD THE DATA
 DTR, LTR = load("data/Train.txt", [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10], 11)
@@ -27,17 +28,24 @@ labels = [
     "good quality"
 ]
 
+# SET SAVE = TRUE TO SAVE AND DON'T VIEW FIGURES
+# SET SAVE = FALSE TO VIEW FIGURES AND DON'T SAVE
+SAVE = False
+
 # histogram
-plotHist(DTR, LTR, features, labels, "DTR's histogram", save=False)
+plotHist(DTR, LTR, features, labels, "Train dataset", save=SAVE)
 
 DTR_gauss, mu, sigma = normalize(DTR)
 
-plotHist(DTR_gauss, LTR, features, labels,
-         "DTR's histogram (normalized)", save=False)
+plotHist(DTR_gauss, LTR, features, labels, "Normalized Train dataset", save=SAVE)
 
-plotHist(DTE, LTE, features, labels, "DTE's histogram", save=False)
+# labels (sum of data for each class)
+plotLabels(LTR, labels, save=SAVE)
 
-DTE_gauss, _mu, _sigma = normalize(DTE, mu, sigma)
 
-plotHist(DTE_gauss, LTE, features, labels,
-         "DTE's histogram (normalized)", save=False)
+# scatter with pca m=2 (2 dimensions)
+DTR_pca, P = pca(DTR, 2)
+DTR_gauss_pca, P_gauss = pca(DTR_gauss, 2)
+
+plot_scatter(DTR_pca, LTR, "PCA m=2", save=SAVE)
+plot_scatter(DTR_gauss_pca, LTR, "Norm + PCA  m=2", save=SAVE)
--- projectFiles/utilityML/Functions/plot.py
+++ projectFiles/utilityML/Functions/plot.py
@@ -1,3 +1,4 @@
+from turtle import color
 import matplotlib.pyplot as plt
 import numpy
 from utilityML.Functions.genpurpose import *
@@ -6,30 +7,47 @@ def plotHist(D, L, hFea, hLab, title, save=False):
     nLabels = len(set(L))
     for i in range(len(hFea)):
         plt.figure()
-        plt.title(title)
+        plt.title(title+": Feature {}".format(i+1))
         plt.xlabel(hFea[i])
         for j in range(nLabels):
             plt.hist(D[:, L == j][i, :],
                      density=True, label=hLab[j], alpha=0.5)
         plt.legend()
         plt.tight_layout()
-    if save:
-        save_plot('hist_%s.pdf' % title)
-    plt.show()
+        if save:
+            save_plot('hist_{}_f{}.pdf'.format(title, i+1))
+    if not save: plt.show()
 
+def plotLabels(L, labels, save=False):
+    class_0 = 0
+    class_1 = 0
 
-def plot_scatter(DP, xLab, yLab, title, save=False):
+    for i in range(len(L)):
+        if L[i] == 0: class_0+=1
+        else: class_1+=1 
+
+    plt.figure()
+    plt.title("Labels")
+    plt.bar(labels, [class_0, class_1], color= ["r", "b"], width=0.3)
+    plt.legend()
+    plt.tight_layout()
+    if save: save_plot("labels.pdf")
+    else: plt.show()
+
+def plot_scatter(DP, L, title, save=False):
+
+    class_0 = DP[:, L < 1]
+    class_1 = DP[:, L > 0]
+
     plt.figure()
     plt.title(title)
-    plt.xlabel(xLab)
-    plt.ylabel(yLab)
-    plt.legend()
+    plt.scatter(class_0[0], class_0[1], marker='x')
+    plt.scatter(class_1[0], class_1[1], marker='x')
     plt.tight_layout()  # Use with non-default font size to keep axis label inside the figure
-    plt.scatter(DP[0, :], DP[1, :])
     
     if save:
         save_plot('scatter_%s.pdf' % (title))
-    plt.show()
+    if not save: plt.show()
 
 def plot_scatter_dual(D, L, hFea, hLab, title, save=False):
 
@@ -113,4 +131,4 @@ def plot_bayes_error(p, DCF, NAMES, colors=None, save=False, legend=None):
 
 
 def save_plot(name):
-    plt.savefig(name)
+    plt.savefig('./img/'+name)
